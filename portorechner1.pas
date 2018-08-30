b6c129ecdcead8871a2efe0851e6ec8175bbd468
Lab247 <42779897+Lab247@users.noreply.github.com>
2018-08-30 21:00:42 +0200

    Add files via upload
    added case of statement

--- portorechner1.pas
+++ portorechner1.pas
@@ -59,40 +59,51 @@ begin
      if error = 0 then // Wenn Error 0 dann Integer Wert vorhanden
         begin
           pnlPorto.visible := True; // pnl anzeigen
-          if gewicht <= 20 then     // Gewicht entspr. Portoklassen prüfen
-             begin
-               pnlPorto.caption := 'Standardbrief 0,70 €';  // pnl Caption entspr. setzen
-             end
-          else
-              begin
-                 if gewicht <= 50 then
-                    begin
-                       pnlPorto.caption := 'Kompaktbrief 0,85 €';
-                    end
-                 else
-                     begin
-                        if gewicht <= 500 then
-                           begin
-                            pnlPorto.caption := 'Großbrief 1,45 €';
-                           end
-                        else
-                            begin
-                               if gewicht <= 1000 then
-                                  begin
-                                   pnlPorto.caption := 'Maxibrief 2,60 €';
-                                  end
-                               else
-                                   begin
-                                      if gewicht <= 2000 then
-                                         begin
-                                              pnlPorto.caption := 'Maxibrief Plus 4,80 €';
-                                         end
-                                      else // über 2000 kein Brief mehr
-                                          pnlPorto.caption := 'Paketrechner benutzen!';
-                                   end
-                            end
-                     end
-              end
+//Variante mit verschachtelten IF Statements
+          //if gewicht <= 20 then     // Gewicht entspr. Portoklassen prüfen
+          //   begin
+          //     pnlPorto.caption := 'Standardbrief 0,70 €';  // pnl Caption entspr. setzen
+          //   end
+          //else
+          //    begin
+          //       if gewicht <= 50 then
+          //          begin
+          //             pnlPorto.caption := 'Kompaktbrief 0,85 €';
+          //          end
+          //       else
+          //           begin
+          //              if gewicht <= 500 then
+          //                 begin
+          //                  pnlPorto.caption := 'Großbrief 1,45 €';
+          //                 end
+          //              else
+          //                  begin
+          //                     if gewicht <= 1000 then
+          //                        begin
+          //                         pnlPorto.caption := 'Maxibrief 2,60 €';
+          //                        end
+          //                     else
+          //                         begin
+          //                            if gewicht <= 2000 then
+          //                               begin
+          //                                    pnlPorto.caption := 'Maxibrief Plus 4,80 €';
+          //                               end
+          //                            else // über 2000 kein Brief mehr
+          //                                pnlPorto.caption := 'Paketrechner benutzen!';
+          //                         end
+          //                  end
+          //           end
+          //    end
+// Variante mit CASE OF Statment
+          CASE gewicht OF
+            0..20 : pnlPorto.caption := 'Standardbrief 0,70 €';  // pnl Caption entspr. setzen
+            21..50 : pnlPorto.caption := 'Kompaktbrief 0,85 €';
+            51..500: pnlPorto.caption := 'Großbrief 1,45 €';
+            501..1000 : pnlPorto.caption := 'Maxibrief 2,60 €';
+            1001..2000 : pnlPorto.caption := 'Maxibrief Plus 4,80 €';
+          else pnlPorto.caption := 'Paketrechner benutzen!';
+          end;
+//
         end
      else // Wenn Error <> 0 kein Integer -> keine Anzeige
           pnlPorto.visible := False;
